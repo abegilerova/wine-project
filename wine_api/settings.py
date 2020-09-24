86cd91c32530495c7711c5a5f02643b71392784d
abegilerova <abegilerova@gmail.com>
2020-09-23 20:36:37 -0500

    added cors

--- wine_api/settings.py
+++ wine_api/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+CORS_ORIGIN_WHITELIST = ('your-full-site-no-https-//.herokuapp.com')
 
 
 # Quick-start development settings - unsuitable for production
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
